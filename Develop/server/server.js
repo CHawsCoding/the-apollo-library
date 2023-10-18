877e0a3d94b0da9f0af8b828377e9fa88037c007
Collin Haws <117249176+CHawsCoding@users.noreply.github.com>
2023-10-18 09:21:13 -0600

    modified server.js to import the necessary apollo server libraries, import our typeDefs and resolvers.js, implemented apolloserver

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,7 +1,10 @@
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const typeDefs = require('./Schemas/typeDefs');
+const resolvers = require('./Schemas/resolvers');
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,8 +17,14 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers, 
+  context: authMiddleware 
+});
+
+server.applyMiddleware({ app });
