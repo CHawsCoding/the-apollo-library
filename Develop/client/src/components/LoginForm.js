24a028017cd259f71d0072715ecc7ea83be44fc0
Collin Haws <117249176+CHawsCoding@users.noreply.github.com>
2023-10-18 11:32:26 -0600

    updated signup and login form files to used GraphQL according to instructions from the project readme

--- Develop/client/src/components/LoginForm.js
+++ Develop/client/src/components/LoginForm.js
@@ -2,11 +2,13 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../graphql/mutations';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN_USER);
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
@@ -26,7 +28,8 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await login({ variables: { ...userFormData } });
+      Auth.login(data.login.token);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
